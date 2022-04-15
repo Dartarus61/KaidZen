6c1a81bfc3572e25cfa8241b113a00ca9774993c
KoMMentatoR1 <leshasherbina@gmail.com>
2022-04-15 08:56:43 +0300

    tryfix

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "https://kaidzen-server.herokuapp.com"
 const $api = axios.create({
-    withCredentials: true,
+    withCredentials: false,
     baseURL: API_URL,
 })
 
